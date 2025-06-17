4103fa6482603fc1eea3efd940498a144b00afe2
KevinSimbana04 <simbanaalexis2004@gmail.com>
2025-06-16 23:36:20 -0500

    Ejercicio_12_KevinS

---
+++ src/Ejercicio12/Cliente.java
@@ -0,0 +1,29 @@
+package Ejercicio12;
+
+public class Cliente {
+    //Atributos de la clase
+    public int codigoCliente;
+    public boolean activo = true;
+    private String nombre;
+    private String telefono ;
+
+    //metodo para que se pueda modificar el atributo desde otra clase
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //metodo para que se pueda obtener el atributo desde otra clase
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
