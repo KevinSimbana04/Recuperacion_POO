6ed1359dd0238c7880fc8e0c9c1aaac2918efea7
KevinSimbana04 <simbanaalexis2004@gmail.com>
2025-06-16 21:23:52 -0500

    Ejercicio_1_KevinS

--- src/Practica_1/Producto.java
+++ src/Practica_1/Producto.java
@@ -1,4 +1,13 @@
 package Practica_1;
 
 public class Producto {
+
+    //Atributos de la clase  con valor cepillo
+    private String nombre="cepillo";
+
+    //metodo para que se puede visualizar  el atributo
+    //desde otra clase
+    public String getNombre() {
+        return nombre;
+    }
 }
