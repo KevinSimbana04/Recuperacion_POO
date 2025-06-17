196ccbc6aaeeef290078eae791bcb8bc32542a1f
KevinSimbana04 <simbanaalexis2004@gmail.com>
2025-06-16 21:54:13 -0500

    Ejercicio_2_KevinS

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
 import Practica_1.Producto;
+import Practica_2.Estudiante;
 
 
 public class Main {
     public static void main(String[] args) {
+        //ejercicio1();
+        ejercicio2();
 
-        //Instanciar  el objeto
-        //Ejercicio1
+
+    }
+    //Ejercicio1
+    public static void ejercicio1(){
+        //instanciar el objeto
         Producto pro=new Producto();
 
-        //Imprimir el metodod
+        //Imprimir el metodo
         System.out.println("-------------Producto--------");
         System.out.println("nombre:" +pro.getNombre());
     }
+    //Ejercicio2
+    public static void ejercicio2(){
+        //instanciar el objeto
+        Estudiante es=new Estudiante();
+
+        //modificar atribujo
+        es.setMatricula("2025A001");
+        System.out.println("La matricula fue registrado correctamente");
+    }
+
 }
---
+++ src/Practica_2/Estudiante.java
@@ -0,0 +1,13 @@
+package Practica_2;
+
+public class Estudiante {
+    //Atributo de la clase
+    private String matricula;
+
+    //metodo para que se puede modificar el atributo
+    //desde otra clase
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}
